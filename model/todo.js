c4a39dcd8c02b1ac8e20fcf25f4b616f8326b0ca
Ali Jahankah <aral2@yahoo.com>
2022-09-01 23:52:02 +0100

    Learning sequelize

--- model/todo.js
+++ model/todo.js
@@ -1,50 +1,22 @@
-const todosMethods = require("../utils/todos");
+const { DataTypes } = require("sequelize");
 
-class Todo {
-  constructor(id, text, completed = false) {
-    this.id = id;
-    this.text = text;
-    this.completed = completed;
-  }
-  save(callback) {
-    todosMethods.getTodos((todos) => {
-      todos.push(this);
-      todosMethods.saveTodos(todos, (err) => {
-        callback(err);
-      });
-    });
-  }
-  static getAllTodos(callback) {
-    todosMethods.getTodos((todos) => {
-      const comps = todos.filter((item) => item.completed === true);
-      const nonComps = todos.filter((item) => item.completed === false);
-      callback({
-        todos,
-        comps,
-        nonComps,
-      });
-    });
-  }
-  static deleteTodo(id, callback) {
-    todosMethods.getTodos((todos, err) => {
-      todosMethods.saveTodos(
-        todos.filter((item) => item.id != id),
-        (err) => {
-          callback(err);
-        }
-      );
-    });
-  }
-  static completeTodo(id, callback) {
-    todosMethods.getTodos((todos, err) => {
-      const todosIndex = todos.findIndex((item) => item.id == id);
-      const newTodo = todos[todosIndex];
-      newTodo.completed = !todos[todosIndex].completed;
-      todos[todosIndex] = newTodo;
-      todosMethods.saveTodos(todos, (err) => {
-        callback(err);
-      });
-    });
-  }
-}
+const sequelize = require("../utils/database");
+
+const Todo = sequelize.define("Todo", {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  text: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  completed: {
+    type: DataTypes.BOOLEAN,
+    allowNull: true,
+    defaultValue: false,
+  },
+});
+
 module.exports = Todo;
---
+++ utils/database.js
@@ -0,0 +1,7 @@
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize("todo_db", "root", "111111", {
+  dialect: "mysql",
+  host: "localhost",
+});
+
+module.exports = sequelize;
