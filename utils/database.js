1b2790da2d9fa7fa781e14a6631be3bca4a75947
Ali Jahankah <aral2@yahoo.com>
2022-09-12 22:35:14 +0100

    remove warnings

--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://localhost/todo_db", {})
+  .connect("mongodb://localhost/todo_db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connected to Database");
   })
